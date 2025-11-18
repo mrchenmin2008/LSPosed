ab125bae9951f97ab098fbae6673908c4ce202fc
chenmin <chenmin@ntimespace.com>
2025-11-18 11:15:31 +0800

    modify lsp api make error2

--- core/src/main/java/android/app/AndroidAppHelper.java
+++ core/src/main/java/android/app/AndroidAppHelper.java
@@ -20,12 +20,12 @@
 
 package android.app;
 
-import static de.robv.android.xposed.XposedHelpers.findClass;
-import static de.robv.android.xposed.XposedHelpers.findFieldIfExists;
-import static de.robv.android.xposed.XposedHelpers.findMethodExactIfExists;
-import static de.robv.android.xposed.XposedHelpers.getObjectField;
-import static de.robv.android.xposed.XposedHelpers.newInstance;
-import static de.robv.android.xposed.XposedHelpers.setFloatField;
+import static de.robv.android.pmxped.PmxpedHelpers.findClass;
+import static de.robv.android.pmxped.PmxpedHelpers.findFieldIfExists;
+import static de.robv.android.pmxped.PmxpedHelpers.findMethodExactIfExists;
+import static de.robv.android.pmxped.PmxpedHelpers.getObjectField;
+import static de.robv.android.pmxped.PmxpedHelpers.newInstance;
+import static de.robv.android.pmxped.PmxpedHelpers.setFloatField;
 
 import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
@@ -37,8 +37,8 @@ import android.view.Display;
 import java.lang.ref.WeakReference;
 import java.util.Map;
 
-import de.robv.android.xposed.XSharedPreferences;
-import de.robv.android.xposed.XposedBridge;
+import de.robv.android.pmxped.XSharedPreferences;
+import de.robv.android.pmxped.PmxpedBridge;
 
 /**
  * Contains various methods for information about the current app.
@@ -71,7 +71,7 @@ public final class AndroidAppHelper {
 		try {
 			return newInstance(CLASS_RESOURCES_KEY, resDir, splitResDirs, overlayDirs, libDirs, displayId, overrideConfiguration, compatInfo);
 		} catch (Throwable t) {
-			XposedBridge.log(t);
+			PmxpedBridge.log(t);
 			return null;
 		}
 	}
--- core/src/main/java/android/content/res/XModuleResources.java
+++ core/src/main/java/android/content/res/XModuleResources.java
@@ -23,10 +23,10 @@ package android.content.res;
 import android.app.AndroidAppHelper;
 import android.util.DisplayMetrics;
 
-import de.robv.android.xposed.IXposedHookInitPackageResources;
-import de.robv.android.xposed.IXposedHookZygoteInit;
-import de.robv.android.xposed.IXposedHookZygoteInit.StartupParam;
-import de.robv.android.xposed.callbacks.XC_InitPackageResources.InitPackageResourcesParam;
+import de.robv.android.pmxped.IPmxpedHookInitPackageResources;
+import de.robv.android.pmxped.IPmxpedHookZygoteInit;
+import de.robv.android.pmxped.IPmxpedHookZygoteInit.StartupParam;
+import de.robv.android.pmxped.callbacks.XC_InitPackageResources.InitPackageResourcesParam;
 import hidden.HiddenApiBridge;
 
 /**
@@ -41,8 +41,8 @@ public class XModuleResources extends Resources {
 	 * Creates a new instance.
 	 *
 	 * <p>This is usually called with {@link StartupParam#modulePath} from
-	 * {@link IXposedHookZygoteInit#initZygote} and {@link InitPackageResourcesParam#res} from
-	 * {@link IXposedHookInitPackageResources#handleInitPackageResources} (or {@code null} for
+	 * {@link IPmxpedHookZygoteInit#initZygote} and {@link InitPackageResourcesParam#res} from
+	 * {@link IPmxpedHookInitPackageResources#handleInitPackageResources} (or {@code null} for
 	 * system-wide replacements).
 	 *
 	 * @param path The path to the APK from which the resources should be loaded.
